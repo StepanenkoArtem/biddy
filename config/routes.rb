37732b7df30b79aeffde181c070819ed23be3940
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2023-12-17 22:30:14 +0200

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
-  # Defines the root path route ("/")
-  root 'pages#home'
+  root to: 'pages#home'
 end
