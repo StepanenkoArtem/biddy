8d7c8b5461b842520a9831c67498b66e31a4189b
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2023-12-22 19:06:46 +0200

    config GA

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :omniauthable, :registerable,       :recoverable, :rememberable, :validatable,  omniauth_providers: [:google]
+  devise :database_authenticatable, :omniauthable, :registerable,       :recoverable, :rememberable, :validatable #,  omniauth_providers: [:google]
 
 end
